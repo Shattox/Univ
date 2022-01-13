b875ed178c60926cae686bf771620ea897239fa3
Shattox <Stevechen77000@gmail.com>
2022-01-13 16:28:53 +0100

    add perl folder

---
+++ M1/S2/Perl/TP1/mystere.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+my $nntpmntp = 10;
+while($nntpnmtp>5) {
+   print "$nntpmntp\n";
+   $nntpmntp --;
+}
+
+my $x = 'oui';
+my $y = 'non';
+if( $x == $y )
+   print "c'est dingue!\n";
+else
+   print "tout va bien\n";
+
+print '2'.'9'+'5'."\n";
