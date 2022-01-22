f45babba64a5aeb7f20b219a6a557f99114854a0
Steve Chen <stevechen77000@gmail.com>
2022-01-22 21:27:36 +0100

    end ex4 TP2 Perl

--- M1/S2/Perl/TP2/ex4.pl
+++ M1/S2/Perl/TP2/ex4.pl
@@ -19,11 +19,36 @@ while (defined(my $line = <$fd>)) {
             $errors++;
         }
         $volumes += $volume;
+        if (exists($urls{$url})) {
+            $urls{$url} ++;
+        } else {
+            $urls{$url} = 1;
+        }
+        if (exists($ips{$ip})) {
+            $ips{$ip} = [$ips{$ip}[0] + 1, $ips{$ip}[1] + $volume];
+        } else {
+            $ips{$ip} = [1, $volume];
+        }
     }
 }
 
 close($fd);
 
-print("$requests\n");
-print("$errors\n");
-print("$volumes\n");
+print("Requests : $requests\n");
+print("Errors : $errors\n");
+print("Volume : $volumes\n\n");
+
+foreach my $url (sort {$urls{$b} <=> $urls{$a}} keys %urls) {
+    print("$url : $urls{$url}\n");
+}
+
+print("\n");
+
+my $counter = 0;
+
+foreach my $ip (sort {$ips{$b}[0] <=> $ips{$a}[0]} keys %ips) {
+    if ($counter++ == 10) { last; }
+    my $access = $ips{$ip}[0];
+    my $volume = $ips{$ip}[1];
+    print("$ip : access=$access volume=$volume\n");
+}
